cedded91c6d6f6a31df3ef5dee7f260094a2527b
nurarifin256 <arifinnur402@gmail.com>
2021-10-28 20:43:20 +0700

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import RegisterBg from "./image/register-bg.jpg";
+import LoginBg from "./image/login.jpg";
+
+export { RegisterBg, LoginBg };
---
+++ src/pages/CreateBlog/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateBlog = () => {
+  return (
+    <div>
+      <p>create blog</p>
+    </div>
+  );
+};
+
+export default CreateBlog;
