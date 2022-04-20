4e276fb304fbee75682807575c9b2f4aba3352e7
nurarifin256 <arifinnur402@gmail.com>
2022-04-20 12:56:10 +0700

    post data & image

--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -2,19 +2,44 @@ import React from "react";
 import { Button, Gap, Input, TextArea, Upload, Link } from "../../components";
 import "./CreateBlog.scss";
 import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { postToAPI, setForm, setImgPreview } from "../../config/redux/action";
 
 const CreateBlog = () => {
+  const { form, imgPreview } = useSelector((state) => state.createBlogReducer);
+  const { title, body } = form;
+  const dispatch = useDispatch();
+
   const history = useHistory();
+
+  const onSubmit = () => {
+    // console.log(title);
+
+    postToAPI(form);
+  };
+
+  const onImageUpload = (e) => {
+    const file = e.target.files[0];
+    dispatch(setForm("image", file));
+    dispatch(setImgPreview(URL.createObjectURL(file)));
+  };
   return (
     <div className="blog-post">
       <Link title="Kembali" onClick={() => history.push("/")} />
       <p className="title">Create New Blog</p>
-      <Input label="Post Title" />
-      <Upload />
-      <TextArea />
+      <Input
+        label="Post Title"
+        value={title}
+        onChange={(e) => dispatch(setForm("title", e.target.value))}
+      />
+      <Upload onChange={(e) => onImageUpload(e)} img={imgPreview} />
+      <TextArea
+        value={body}
+        onChange={(e) => dispatch(setForm("body", e.target.value))}
+      />
       <Gap height={10} />
       <div className="button-action">
-        <Button title="Save" />
+        <Button title="Save" onClick={onSubmit} />
       </div>
       <Gap height={20} />
     </div>
